d301923da254f9165b970562b63c29aa8898dc84
christophe <christophebuffe@gmail.com>
2022-04-14 15:26:42 +0200

    full clean of notesJsx component

--- doredo/src/components/notes/notes.jsx
+++ doredo/src/components/notes/notes.jsx
@@ -1,47 +1,14 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { generateColor, onClickEvent } from "../../config/fonctions";
 
 const Notes = ({ 
-  nButtons, 
-  score, 
-  points, 
-  remainingTime,
+  nButtons,  
   coloredButtonIndex,
-  setColoredButtonIndex,
   currentRandomColor,
-  setCurrentRandomColor,
-  currentScore,
-  setCurrentScore,
-  currentTime,
-  setCurrentTime
+  onClick
 }) => {
-
-  useEffect(() => {
-    let chrono = null;
-    chrono = setInterval(() => {
-      setCurrentTime((currentTime) => currentTime === 0 ? clearInterval(chrono) : currentTime - 1);
-    }, 1000);
-  }, []); 
-
-  useEffect(() => {
-    if(currentTime === 0) {
-      alert("vous avez perdu, il vous reste " + currentScore + " boutton ")
-    }
-    if(currentScore === 0) {
-      alert("Félicitation vous avez cliqué sur tous les boutons dans le temps impartit, il restait encore " + currentTime + " secondes");
-      setCurrentScore(score);
-    }
-  }, [currentTime, currentScore]);
-
-  useEffect(() => {
-    generateColor(setCurrentRandomColor);
-  }, [coloredButtonIndex]);
 
   return (
     <div className="container-notes">
-      <p>Il vous reste <strong>{currentScore}</strong> boutons à clicker... click click click</p>
-      <p>Chrono: {currentTime}</p>
       {
         [...Array(nButtons)].map((value, index) => {
           const currentButtonStyle = {backgroundColor: ""}
@@ -53,7 +20,9 @@ const Notes = ({
               type="button"
               key={index}
               style={currentButtonStyle}
-              onClick={() => onClickEvent(coloredButtonIndex === index, setColoredButtonIndex, nButtons, setCurrentScore, currentScore, points)} 
+              onClick={() => {
+                onClick(index);
+              }}
             />
           )
         })
--- doredo/src/pages/game.jsx
+++ doredo/src/pages/game.jsx
@@ -11,33 +11,50 @@ import {
   remainingTime } from "../config/config";
 import {
    generateColor,
-   onClickEvent
- } from "../config/fonctions";
+   onClickEvent } from "../config/fonctions";
 
  //Import components
 import Notes from "../components/notes/notes";
 
 const Game = () => {
+  
   const [colorNoteIndex, setColorNoteIndex] = useState(startNote);
   const [currentRandomColor, setCurrentRandomColor] = useState("");
   const [currentScore, setCurrentScore] = useState(score);
   const [currentTime, setCurrentTime] = useState(remainingTime);
 
+  useEffect(() => {
+    //Peut être déclarer chrono en dehors du scope pour l'arrêter aussi quand le joueur remplis les conditions de victoire. Cependant j'envisage de faire cliquer un nombre de bouton déterminé puis comparer le temps.
+    let chrono = null;
+    chrono = setInterval(() => {
+      setCurrentTime((currentTime) => currentTime === 0 ? clearInterval(chrono) : currentTime - 1);
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if(currentTime === 0) {
+      alert("Vous avez perdu, il vous reste " + currentScore + " bouttons");
+    }
+    if(currentScore === 0) {
+      alert("Félicitation vous avez cliqué sur tous les boutons dans le temps impartit, il restait encore " + currentTime + " secondes");
+      setCurrentScore(score);
+      // clearInterval(chrono);
+    }
+  }, [currentTime, currentScore]);
+
+  useEffect(() => {
+    generateColor(setCurrentRandomColor);
+  }, [colorNoteIndex])
+
   return (
     <div className="container-game">
+      <p>Il vous reste <strong>{currentScore}</strong> boutons à cliquer... click click click</p>
+      <p>Chrono: {currentTime}</p>
       <Notes
          nButtons={nNotes}
-         score={score}
-         points={points}
-         remainingTime={remainingTime}
          coloredButtonIndex={colorNoteIndex}
-         setColoredButtonIndex={setColorNoteIndex}
          currentRandomColor={currentRandomColor}
-         setCurrentRandomColor={setCurrentRandomColor}
-         currentScore={currentScore}
-         setCurrentScore={setCurrentScore}
-         currentTime={currentTime}
-         setCurrentTime={setCurrentTime}
+         onClick={(index) => onClickEvent(colorNoteIndex === index, setColorNoteIndex, nNotes, setCurrentScore, currentScore, points)}
       />
     </div>
   );
